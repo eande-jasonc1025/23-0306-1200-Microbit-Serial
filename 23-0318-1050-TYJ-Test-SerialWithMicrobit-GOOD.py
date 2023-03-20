411c31e375b44e2353cd8bccfd29090422eb152e
eande-jasonc1025 <eande.jasonc@gmail.com>
2023-03-19 20:06:59 -0700

    first commit

---
+++ 23-0318-1050-TYJ-Test-SerialWithMicrobit-GOOD.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import time
+import serial
+from datetime import datetime
+from csv import writer
+import http.server
+
+ser = serial.Serial(
+        ##jwc o port='/dev/ttyACM0',
+        port='COM3',
+        baudrate = 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+while True:
+    x=ser.readline()
+    if x:
+        dt = datetime.now()
+        datestamp = str(dt)[:16]
+        ###jwc o temp, light = x.decode().split(':')
+        ###jwc 23-0310-1120 y id, te, li, co = x.decode().split(',')
+        ###jwc n id, te, li, co, m1, m2, m3, m4 = x.decode().split('|')
+
+        ###jwc o newData = [datestamp,temp,light]
+        ###jwc 23-0310-1120 y newData = [datestamp, id, te, li, co]
+        ###jwc n newData = [datestamp, id, te, li, co, m1, m2, m3, m4]
+        newData = [datestamp, x]
+
+        print(newData)
+        with open('test.csv', 'a', newline='') as f_object:
+            writer_object = writer(f_object)
+            writer_object.writerow(newData)
+            f_object.close()
